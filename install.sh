e765f838208197fd3b46180139022e3b59d2cd7f
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-04-25 15:26:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-sudo apt-get install aapt wget python3 python3-pip zipalign imagemagick openjdk-8-jdk -y
+apt install aapt wget python3 python3-pip zipalign imagemagick openjdk-8-jdk -y
 wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
 wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar
-sudo mv apktool /usr/bin/apktool && sudo chmod +x /usr/bin/apktool
-sudo mv apktool_2.8.1.jar /usr/bin/apktool.jar && sudo chmod +x /usr/bin/apktool.jar
+mv apktool /usr/bin/apktool && chmod +x /usr/bin/apktool
+mv apktool_2.8.1.jar /usr/bin/apktool.jar &&  chmod +x /usr/bin/apktool.jar
 pip install Pillow
